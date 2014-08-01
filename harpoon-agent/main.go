a9fd8ffa8190ad70003b679ed5071e3d0187ccca
Jeff Younker <jeff@drinktomi.com>
2014-08-01 20:09:50 +0200

    Recieve logs was not launched as a goroutine.

--- harpoon-agent/main.go
+++ harpoon-agent/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -52,7 +53,7 @@ func main() {
 		api = newAPI(r)
 	)
 
-	receiveLogs(r)
+	go receiveLogs(r)
 	http.Handle("/", api)
 
 	go func() {
