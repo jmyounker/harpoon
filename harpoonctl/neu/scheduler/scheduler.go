76bb59eebe779aa3f4918cf57baa9e0a34f62084
Peter Bourgon <peter@bourgon.org>
2014-11-12 14:46:41 +0100

    Clean up scheduler top-level

--- harpoonctl/neu/scheduler/scheduler.go
+++ harpoonctl/neu/scheduler/scheduler.go
@@ -2,13 +2,11 @@ package scheduler
 
 import "github.com/codegangsta/cli"
 
-var Verbosef = func(string, ...interface{}) {}
-
 // Command is the scheduler subcommand.
 var Command = cli.Command{
 	Name:        "scheduler",
-	Usage:       "Control a Harpoon scheduler",
-	Description: "Interact with a Harpoon scheduler directly.",
+	Usage:       "Control a Harpoon agent",
+	Description: "Interact with a Harpoon scheduler.",
 	Subcommands: []cli.Command{},
 	Flags:       []cli.Flag{},
 	HideHelp:    true,
