e8c80691f6a292a13c86c40277629f51e19ed642
Peter Bourgon <peter@bourgon.org>
2014-11-19 17:23:48 +0100

    harpoonctl: agent ps use general function

--- harpoonctl/agent/ps.go
+++ harpoonctl/agent/ps.go
@@ -2,9 +2,9 @@ package agent
 
 import (
 	"fmt"
+	"io"
 	"net/url"
 	"os"
-	"sort"
 	"text/tabwriter"
 
 	"github.com/codegangsta/cli"
@@ -24,17 +24,54 @@ var psCommand = cli.Command{
 func psAction(c *cli.Context) {
 	var (
 		w  = tabwriter.NewWriter(os.Stdout, 0, 2, 2, ' ', 0)
-		l  = c.Bool("long")
-		f  = func(host string, m map[string]agent.ContainerInstance) []string { return []string{} }
-		ch = make(chan []string, len(endpoints))
+		ch = make(chan map[string]map[string]agent.ContainerInstance, len(endpoints)) // agent: container ID: instance
+		m  = map[string]map[string]agent.ContainerInstance{}                          // merged
 	)
 
-	if l {
+	for _, u := range endpoints {
+		go func(u *url.URL) {
+			m := map[string]map[string]agent.ContainerInstance{}
+
+			defer func() { ch <- m }()
+
+			client, err := agent.NewClient(u.String())
+			if err != nil {
+				log.Warnf("%s: %s", u.Host, err)
+				return
+			}
+
+			containers, err := client.Containers()
+			if err != nil {
+				log.Warnf("%s: %s", u.Host, err)
+				return
+			}
+
+			m = map[string]map[string]agent.ContainerInstance{u.Host: containers}
+		}(u)
+	}
+
+	for i := 0; i < cap(ch); i++ {
+		for host, containers := range <-ch {
+			m[host] = containers
+		}
+	}
+
+	WriteContainerPS(w, m, c.Bool("long"))
+}
+
+// WriteContainerPS writes a tab-delimited `ps` output for the containers to
+// the passed writer.
+func WriteContainerPS(w writeFlusher, m map[string]map[string]agent.ContainerInstance, long bool) {
+	if len(m) <= 0 {
+		return
+	}
+
+	if long {
 		fmt.Fprint(w, "AGENT\tID\tSTATUS\tCPUTIME\tMEM USED\tFDS\tRESTARTS\tOOMS\tCMD\tPORTS\tRC\n")
-		f = func(host string, m map[string]agent.ContainerInstance) []string {
-			a := make([]string, 0, len(m))
-			for id, ci := range m {
-				a = append(a, fmt.Sprintf(
+		for host, containers := range m {
+			for id, ci := range containers {
+				fmt.Fprintf(
+					w,
 					"%s\t%s\t%s\t%d\t%dM\t%d\t%d\t%d\t%s\t%+v\t%d\n",
 					host,
 					id,
@@ -47,69 +84,29 @@ func psAction(c *cli.Context) {
 					ci.Command.Exec[0],
 					ci.Ports,
 					ci.ExitStatus,
-				))
+				)
 			}
-			return a
 		}
 	} else {
 		fmt.Fprint(w, "AGENT\tID\tSTATUS\tCMD\n")
-		f = func(host string, m map[string]agent.ContainerInstance) []string {
-			a := make([]string, 0, len(m))
-			for id, ci := range m {
-				a = append(a, fmt.Sprintf(
+		for host, containers := range m {
+			for id, ci := range containers {
+				fmt.Fprintf(
+					w,
 					"%s\t%s\t%s\t%s\n",
 					host,
 					id,
 					ci.ContainerStatus,
 					ci.Command.Exec[0],
-				))
+				)
 			}
-			return a
 		}
-	}
-
-	for _, u := range endpoints {
-		go func(u *url.URL) {
-			var m map[string]agent.ContainerInstance
-			defer func() { ch <- f(u.Host, m) }()
-
-			c, err := agent.NewClient(u.String())
-			if err != nil {
-				log.Warnf("%s: %s", u.Host, err)
-				return
-			}
-
-			m, err = c.Containers()
-			if err != nil {
-				log.Warnf("%s: %s", u.Host, err)
-				return
-			}
-		}(u)
-	}
-
-	first := make([][]string, cap(ch))
-	for i := 0; i < cap(ch); i++ {
-		first[i] = <-ch
-	}
-
-	a := []string{}
-	for _, slice := range first {
-		for _, s := range slice {
-			a = append(a, s)
-		}
-	}
-
-	// Don't display header if we didn't have any rows.
-	if len(a) <= 0 {
-		log.Verbosef("no tasks")
-		return
-	}
-
-	sort.StringSlice(a).Sort()
-
-	for _, s := range a {
-		fmt.Fprintf(w, s)
 	}
 
 	w.Flush()
+}
+
+type writeFlusher interface {
+	io.Writer
+	Flush() error
 }
