9fce757143d8042487964c3a74ae7c309e59dee8
Peter Bourgon <peter@bourgon.org>
2014-12-15 10:46:15 +0100

    agent: by default, don't emit any SD information

--- harpoon-agent/main.go
+++ harpoon-agent/main.go
@@ -38,7 +38,7 @@ func main() {
 		portsStart        = flag.Uint64("ports.start", 30000, "starting of port allocation range")
 		portsEnd          = flag.Uint64("ports.end", 32767, "ending of port allocation range")
 		downloadTimeout   = flag.Duration("download.timeout", agent.DefaultDownloadTimeout, "max artifact download time")
-		sdFilename        = flag.String("sd.filename", "./harpoon-containers.json", "file to write service information")
+		sdFilename        = flag.String("sd.filename", "", "file to write service information")
 		sdReload          = flag.String("sd.reload", "", "command to execute after writing -sd.filename")
 	)
 	flag.Var(&configuredVolumes, "vol", "repeatable list of available volumes")
@@ -64,9 +64,20 @@ func main() {
 		log.Fatal("port range start must be before port range end")
 	}
 
-	r := newRegistry(newConsulServiceDiscovery(*sdFilename, *sdReload))
+	var sd serviceDiscovery
+	if *sdFilename != "" {
+		log.Printf("emitting service discovery information to %s", *sdFilename)
+		sd = newConsulServiceDiscovery(*sdFilename, *sdReload)
+	} else {
+		log.Printf("not emitting service discovery information")
+		sd = nopServiceDiscovery{}
+	}
+
+	r := newRegistry(sd)
+
 	pdb := newPortDB(portsStart16, portsEnd16)
 	defer pdb.exit()
+
 	api := newAPI(*containerRoot, r, pdb, configuredVolumes, *agentCPU, *agentMem, *downloadTimeout, *debug)
 
 	go receiveLogs(r, *logAddr)
