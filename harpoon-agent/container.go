8e251e0db0230cd4582d992e85f4250c97e85269
Jeff Younker <jeff@drinktomi.com>
2014-12-22 23:44:02 +0100

    Report stderr when untarring artifacts.

--- harpoon-agent/container.go
+++ harpoon-agent/container.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
@@ -542,13 +544,32 @@ func extractArtifact(src io.Reader, dst string, compression string) (err error) 
 		}
 	}()
 
-	cmd := exec.Command("tar", "-C", dst, "-x"+compression)
+	log.Printf("extracting with cmd: tar -xv%s -C %s ", compression, dst)
+	cmd := exec.Command("tar", "-xv"+compression, "-C", dst)
 	cmd.Stdin = src
 
-	if err := cmd.Run(); err != nil {
-		return fmt.Errorf("tar extraction failed: %s", err)
+	// It's incredibly hard to debug failures in tar unless stderr is available.
+	// Fortunately tar only spews to stderr when something goes wrong.
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return errors.New("could not open tar's stderr")
 	}
 
+	if err := cmd.Start(); err != nil {
+		log.Printf("could not start tar command: %s", err)
+	}
+
+	tarout, err := ioutil.ReadAll(stderr)
+	if err != nil {
+		log.Printf("error while from tar's stderr: %s", err)
+	}
+	if len(tarout) != 0 {
+		log.Printf("stderr from tar: %s", string(tarout))
+	}
+
+	if err := cmd.Wait(); err != nil {
+		return fmt.Errorf("tar extraction failed: %s", err)
+	}
 	return nil
 }
 
